507043f1822ff43e806f69aeb4829c16336ba0aa
Vincent Andreas <46801705+imvincentandreas@users.noreply.github.com>
2020-10-04 13:32:40 -0400

    Create insert_certification.sql

---
+++ insert_certification.sql
@@ -0,0 +1,5 @@
+insert into certifications(certificationID, name, validity) values (1, CPR (CPR for Healthcare Providers/American Heart Association), 2);
+insert into certifications(certificationID, name, validity) values (2, CPR (CPR for the Professional Rescuer/American Red Cross), 2);
+insert into certifications(certificationID, name, validity) values (3, Firefighter I (Athens Technical College), 3);
+insert into certifications(certificationID, name, validity) values (4, Firefighter I (Ivy Technical College), 3);
+insert into certifications(certificationID, name, validity) values (5, POST (Georgia POST Academy), 5;
