02d4e01ebf6c3ea946d5ee7de0a65e9331950df8
Vincent Andreas <46801705+imvincentandreas@users.noreply.github.com>
2020-10-04 13:56:26 -0400

    Update insert_memberCertification.sql

--- insert_memberCertification.sql
+++ insert_memberCertification.sql
@@ -1,5 +1,5 @@
-insert into user_certification (memberID, certificateID, earned, expiry) values (1, 3, '2020-03-12', '2025-03-12');
-insert into user_certification (memberID, certificateID, earned, expiry) values (2, 2, '2020-05-03', '2025-05-03');
-insert into user_certification (memberID, certificateID, earned, expiry) values (3, 2, '2020-01-04', '2025-01-04');
-insert into user_certification (memberID, certificateID, earned, expiry) values (4, 5, '2020-09-04', '2025-09-04');
-insert into user_certification (memberID, certificateID, earned, expiry) values (5, 2, '2020-06-12', '2025-06-12');
+insert into user_certification (memberID, certificationID, earn, expiration) values (1, 3, '2020-03-12', '2025-03-12');
+insert into user_certification (memberID, certificationID, earn, expiration) values (2, 2, '2020-05-03', '2025-05-03');
+insert into user_certification (memberID, certificationID, earn, expiration) values (3, 2, '2020-01-04', '2025-01-04');
+insert into user_certification (memberID, certificationID, earn, expiration) values (4, 5, '2020-09-04', '2025-09-04');
+insert into user_certification (memberID, certificationID, earn, expiration) values (5, 2, '2020-06-12', '2025-06-12');
