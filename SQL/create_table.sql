ba24ee05a75919443f9cc7eeadecba71f882b96f
taylorburd26 <70534843+taylorburd26@users.noreply.github.com>
2020-10-09 15:28:58 -0400

    fixed create member sql statement

--- SQL/create_table.sql
+++ SQL/create_table.sql
@@ -6,26 +6,24 @@ CREATE TABLE certification (
     PRIMARY KEY (certificationID))
     ENGINE = INNODB;
 
-CREATE TABLE member (
-    memberID INT(11) AUTO_INCREMENT NOT NULL,
-    fname VARCHAR(256) NOT NULL,
-    mname VARCHAR(256),
-    lname VARCHAR(256),
-    street VARCHAR(256) NOT NULL,
-    city VARCHAR(256) NOT NULL,
-    state VARCHAR(256) NOT NULL,
-    zip VARCHAR(256) NOT NULL,
-    phone VARCHAR(256) NOT NULL,
-    email VARCHAR(256) NOT NULL,
-    position VARCHAR(256) NOT NULL,
-    station VARCHAR(256) NOT NULL,
-    radio_num VARCHAR(256) NOT NULL,
-    account_password VARCHAR(256) NOT NULL,
-    status VARCHAR(256) NOT NULL,
-    PRIMARY KEY (memberID),
-    FOREIGN KEY (positionID) REFERENCES position(positionID),
-    FOREIGN KEY (accountID) REFERENCES account(accountID))
-    ENGINE = INNODB;
+    CREATE TABLE member (
+        memberID INT(11) AUTO_INCREMENT NOT NULL,
+        fname VARCHAR(256) NOT NULL,
+        mname VARCHAR(256),
+        lname VARCHAR(256),
+        street VARCHAR(256) NOT NULL,
+        city VARCHAR(256) NOT NULL,
+        state VARCHAR(256) NOT NULL,
+        zip VARCHAR(256) NOT NULL,
+        phone VARCHAR(256) NOT NULL,
+        email VARCHAR(256) NOT NULL,
+        position VARCHAR(256) NOT NULL,
+        station VARCHAR(256) NOT NULL,
+        radio_num VARCHAR(256) NOT NULL,
+        account_password VARCHAR(256) NOT NULL,
+        status VARCHAR(256) NOT NULL,
+        PRIMARY KEY (memberID))
+        ENGINE = INNODB;
 
 CREATE TABLE user_certification (
     memberID INT(11) NOT NULL,
