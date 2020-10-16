df13a9bb9feda5cc0d8fa2b92901cc51045fa18b
Vincent Andreas <46801705+imvincentandreas@users.noreply.github.com>
2020-10-16 08:46:01 -0400

    added some more back buttons and tweaked viewmemcert

--- app/public/api/certificates/viewmemcert.php
+++ app/public/api/certificates/viewmemcert.php
@@ -5,7 +5,7 @@ require 'common.php';
 $db = DbConnection::getConnection();
 
 $stmt = $db->prepare (
-  "SELECT * FROM member AS m, certification AS c, user_certification AS uc WHERE m.memberID = uc.memberID AND c.certificationID = uc.certificationID"
+  "SELECT * FROM member AS m, certification AS c, user_certification AS uc WHERE m.memberID = uc.memberID AND c.certificationID = uc.certificationID AND expiration > NOW()"
 );
 
 $stmt->execute();
