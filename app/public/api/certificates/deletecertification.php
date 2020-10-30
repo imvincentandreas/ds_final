16d3d109348f679ef19671856e0a26266e7061fd
Vincent Andreas <46801705+imvincentandreas@users.noreply.github.com>
2020-10-29 21:26:54 -0400

    add delete certification

---
+++ app/public/api/certificates/deletecertification.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'common.php';
+
+$db = DbConnection::getConnection();
+
+$stmt = $db->prepare(
+  'DELETE FROM certification WHERE certificationID = ?'
+);
+
+$stmt->execute([
+  $_POST['certificationID']
+]);
+
+header('HTTP/1.1 303 See Other');
+
+header('Location: ../certificates/');
+?>
---
+++ app/public/js/deletecertificates.js
@@ -0,0 +1,41 @@
+var certificateInfo = new Vue({
+  el: '#certificateInfo',
+  data: {
+    certificates: [],
+    certification: {}
+  },
+  methods: {
+    fetchCertificates() {
+      fetch('api/certificates/viewcertificates.php')
+      .then(response => response.json())
+      .then(json => { certificateInfo.certificates = json })
+    },
+
+    handleDeleteCertificateForm() {
+      fetch('api/certificates/deletecertification.php', {
+        method:'POST',
+        body: JSON.stringify(this.certification),
+        headers:{
+          "Content-Type": "application/json; charset=utf-8"
+        }
+        })
+        .then( response => response.text() )
+
+        this.handleData();
+      },
+
+      handleData() {
+        this.certification = {
+          certificationID: ""
+        }
+      }
+  },
+  created() {
+    this.fetchCertificates();
+  }
+});
+
+function success() {
+  alert("Certification deleted successfully, redirecting to view certifications page");
+  window.location.href = "viewCertificates.html";
+}
